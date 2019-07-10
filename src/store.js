896c6ec22cdf203e4428e1a3be55f5477a16854e
Adrianus Cornet <ardjan.cornet@gmail.com>
2019-07-10 10:34:03 +0200

    import redusers to store

--- src/store.js
+++ src/store.js
@@ -5,16 +5,12 @@ import {
 } from 'redux'
 import reduxThunk from 'redux-thunk'
 
-// impo './'
+import reducer from './reducers'
 
 const middleware = applyMiddleware(reduxThunk)
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
 const enhancer = composeEnhancer(middleware)
-
-function reducer (state) {
-  return state
-}
 
 export default createStore(reducer, enhancer)
