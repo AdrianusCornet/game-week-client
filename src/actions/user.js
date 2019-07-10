1be66948cfb53ed99d4fa7751500fb34c55981a0
Adrianus Cornet <ardjan.cornet@gmail.com>
2019-07-10 12:06:03 +0200

    improve new user

--- src/actions/user.js
+++ src/actions/user.js
@@ -2,9 +2,14 @@ export const NEW_USER = 'NEW_USER'
 export const ADD_JWT = 'ADD_JWT'
 export const UPDATE_USER_NAME = 'UPDATE_USER_NAME'
 
-export function newUser(user)  {
+export function newUser(userId, name, jwt = '')  {
+  const newUser = {
+    userId,
+    name,
+    jwt
+  }
   return {
     type: NEW_USER,
-    payload: user
+    payload: newUser
   }
 }
