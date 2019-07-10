08e8c5ed11a0186c71b1b5b5e6f5e51e5057e2f6
Adrianus Cornet <ardjan.cornet@gmail.com>
2019-07-10 12:02:54 +0200

    add reduse new user

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,14 +1,19 @@
-// import actions 
+import { NEW_USER } from '../actions/user'
 
 const initialState = {
+  userId: NaN,
   name: '',
   jwt: '',
 }
 
 export default function user(state = initialState, { type, payload }) {
   switch (type) {
-    case null:
-      return payload
+    case NEW_USER:
+      const newUser = {
+        ...initialState,
+        ...payload
+      }
+      return newUser
     default:
       return state
   }
