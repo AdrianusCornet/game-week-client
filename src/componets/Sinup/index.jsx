94060634b756011d79c86aa6fb7c0d3575f5930c
Adrianus Cornet <ardjan.cornet@gmail.com>
2019-07-11 20:54:27 +0200

    Use heroku server

--- src/componets/Sinup/index.jsx
+++ src/componets/Sinup/index.jsx
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import superagent from 'superagent'
 
-const baseUrl = 'http://localhost:4000'
-// const baseUrl = 'https://game-week-server.herokuapp.com'
+// const baseUrl = 'http://localhost:4000'
+const baseUrl = 'https://game-week-server.herokuapp.com'
 
 export default class index extends Component {
   initialText = {
